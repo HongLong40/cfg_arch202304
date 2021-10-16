7c4eb454d95bdbaf0e9adde0e2c070cdfa2bc952
HongLong40 <37321971+HongLong40@users.noreply.github.com>
2021-10-16 21:06:15 +0200

    config cleanup HLWM/autostart and polybar

--- .config/polybar/info_hlwm_workspaces.sh
+++ .config/polybar/info_hlwm_workspaces.sh
@@ -2,49 +2,49 @@
 
 herbstclient --idle "tag_*" 2>/dev/null | {
 
-    while true; do
+    while true
+    do
+        typeset padding="    " # 4 spaces
+
         # Read tags into $tags as array
         IFS=$'\t' read -ra tags <<< "$(herbstclient tag_status)"
         {
-            for i in "${tags[@]}" ; do
+            for i in "${tags[@]}"
+            do
                 # Read the prefix from each tag and render them according to that prefix
                 case ${i:0:1} in
                     '#')
                         # the tag is viewed on the focused monitor
-                        # TODO Add your formatting tags for focused workspaces
-                        echo "%{B#355B1A}" # Add your formatting tags for empty workspaces
+                        echo "%{F-}%{B#355B1A}"
                         ;;
                     ':')
                         # : the tag is not empty
-                        # TODO Add your formatting tags for occupied workspaces
-                        echo "%{B#5B1A35}" # Add your formatting tags for empty workspaces
+                        echo "%{F-}%{B#5B1A35}"
                         ;;
                     '!')
                         # ! the tag contains an urgent window
-                        # TODO Add your formatting tags for workspaces with the urgent hint
-                        echo "%{B#FF0000}"
+                        echo "%{F-}%{B#FF0000}"
                         ;;
                     '-')
                         # - the tag is viewed on a monitor that is not focused
-                        # TODO Add your formatting tags for visible but not focused workspaces
-                        echo "%{F-}%{B#764E29}" # Add your formatting tags for empty workspaces
+                        echo "%{F-}%{B#764E29}"
                         ;;
                     *)
                         # . the tag is empty
-                        # There are also other possible prefixes but they won't appear here
-                        echo "%{F#AAAAAA}%{B-}" # Add your formatting tags for empty workspaces
+                        echo "%{F#AAAAAA}%{B-}"
                         ;;
                 esac
 
-                echo "%{A1:herbstclient use ${i:1}:}    ${i:1}    %{A -u -o F- B-}"
+                echo "%{A1:herbstclient use ${i:1}:}${padding}${i:1}${padding}%{A F- B-}"
             done
 
-            echo "%{F-}%{B-}"
+            #echo "%{F-}%{B-}"
         } | tr -d "\n"
 
-    echo
+        echo
 
-    # wait for next event from herbstclient --idle
-    read -r || break
-done
+        # wait for next event from herbstclient --idle
+        read -r || break
+    done
+
 } 2>/dev/null
