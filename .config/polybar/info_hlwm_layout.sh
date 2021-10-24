a1cc3c520b3fad898283dfebe35a8496eab498ae
HongLong40 <37321971+HongLong40@users.noreply.github.com>
2021-10-24 13:53:41 +0200

    more polybar layout tweaks

--- .config/polybar/info_hlwm_layout.sh
+++ .config/polybar/info_hlwm_layout.sh
@@ -5,34 +5,7 @@ herbstclient --idle "attribute_changed" 2>/dev/null | {
     while true
     do
         # Read layout of focused frame
-        #_layout=$(herbstclient layout | awk -F ":" '/FOCUS/ { split($1,b," "); print b[2] }')
-        echo $(herbstclient layout | awk -F ":" '/FOCUS/ { split($1,b," "); print toupper(substr(b[2],1,1)) }')
-
-#        {
-#            #echo "%{F#FFFFFF}"
-#
-#            case ${_layout} in
-#                'grid')
-#                    echo "  G  "
-#                    ;;
-#                'horizontal')
-#                    echo "  H  "
-#                    ;;
-#                'max')
-#                    echo "  M  "
-#                    ;;
-#                'vertical')
-#                    echo "  V  "
-#                    ;;
-#                *)
-#                    echo " ??? "
-#                    ;;
-#            esac
-#
-#            echo "%{F-}"
-#        } | tr -d "\n"
-
-        #echo
+        echo $(herbstclient layout | awk '/FOCUS/ { print toupper(substr($2,1,1)) }')
 
         # wait for next event from herbstclient --idle
         read || break
