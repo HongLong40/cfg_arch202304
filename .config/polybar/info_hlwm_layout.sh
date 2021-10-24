65b9ef0f2703cd307469903ff3968ab63aa63d6b
HongLong40 <37321971+HongLong40@users.noreply.github.com>
2021-10-24 12:18:56 +0200

    corrected file name info_hlwm_layout.sh

---
+++ .config/polybar/info_hlwm_layout.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env zsh
+
+herbstclient --idle "attribute_changed" 2>/dev/null | {
+
+    while true
+    do
+        # Read layout of focused frame
+        #_layout=$(herbstclient layout | awk -F ":" '/FOCUS/ { split($1,b," "); print b[2] }')
+        echo $(herbstclient layout | awk -F ":" '/FOCUS/ { split($1,b," "); print toupper(substr(b[2],1,1)) }')
+
+#        {
+#            #echo "%{F#FFFFFF}"
+#
+#            case ${_layout} in
+#                'grid')
+#                    echo "  G  "
+#                    ;;
+#                'horizontal')
+#                    echo "  H  "
+#                    ;;
+#                'max')
+#                    echo "  M  "
+#                    ;;
+#                'vertical')
+#                    echo "  V  "
+#                    ;;
+#                *)
+#                    echo " ??? "
+#                    ;;
+#            esac
+#
+#            echo "%{F-}"
+#        } | tr -d "\n"
+
+        #echo
+
+        # wait for next event from herbstclient --idle
+        read || break
+    done
+
+} 2>/dev/null
