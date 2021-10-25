d5010b27a989300b311b742c3084e77f8dfae7c9
HongLong40 <37321971+HongLong40@users.noreply.github.com>
2021-10-25 23:28:55 +0200

    polybar: simplified event handler for layout changes

--- .config/polybar/info_hlwm_layout.sh
+++ .config/polybar/info_hlwm_layout.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
-echo $(herbstclient layout | awk '/FOCUS/ { print toupper(substr($2,1,1)) }')
+_layout=$(herbstclient get_attr tags.focus.tiling.focused_frame.algorithm)
+echo ${${_layout:0:1}:u}
 
 herbstclient --idle "attribute_changed" 2>/dev/null | {
 
@@ -10,21 +11,5 @@ herbstclient --idle "attribute_changed" 2>/dev/null | {
             echo ${${event[$#event]:0:1}:u}
         fi
     done
-
-#    while read event
-#    do
-#        echo $event | awk ' /focused/ { print toupper(substr($NF,1,1))} '
-#    done
-
-
-
-#    while true
-#    do
-#        # Read layout of focused frame
-#        echo $(herbstclient layout | awk '/FOCUS/ { print toupper(substr($2,1,1)) }')
-#
-#        # wait for next event from herbstclient --idle
-#        read || break
-#    done
 
 } 2>/dev/null
