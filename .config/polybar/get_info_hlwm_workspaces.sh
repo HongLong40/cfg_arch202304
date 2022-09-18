2e572f160df0266d06d5d75f8dbec68c06583b6b
HongLong40 <37321971+HongLong40@users.noreply.github.com>
2022-09-18 14:29:49 +0200

    use echo -n instead of piping to tr

--- .config/polybar/get_info_hlwm_workspaces.sh
+++ .config/polybar/get_info_hlwm_workspaces.sh
@@ -1,38 +1,37 @@
 #!/bin/zsh
 
 typeset padding="    " # 4 spaces
+typeset -a tags
 
 # Read tags into $tags as array
 tags=( $(herbstclient tag_status) )
 
-{
-    for tag in ${tags}
-    do
-        # Read the prefix from each tag and render them according to that prefix
-        case ${tag:0:1} in
-            '#')
-                # the tag is viewed on the focused monitor
-                echo "%{F-}%{B#3A631C}"
-                ;;
-            ':')
-                # : the tag is not empty
-                echo "%{F-}%{B#631C3A}"
-                ;;
-            '!')
-                # ! the tag contains an urgent window
-                echo "%{F-}%{B#FF0000}"
-                ;;
-            '-')
-                # - the tag is viewed on a monitor that is not focused
-                echo "%{F-}%{B#764E29}"
-                ;;
-            *)
-                # . the tag is empty
-                echo "%{F#AAAAAA}%{B-}"
-                ;;
-        esac
+for tag in ${tags}
+do
+    # Read the prefix from each tag and render them according to that prefix
+    case ${tag:0:1} in
+        '#')
+            # the tag is viewed on the focused monitor
+            echo -n "%{F-}%{B#3A631C}"
+            ;;
+        ':')
+            # : the tag is not empty
+            echo -n "%{F-}%{B#631C3A}"
+            ;;
+        '!')
+            # ! the tag contains an urgent window
+            echo -n "%{F-}%{B#FF0000}"
+            ;;
+        '-')
+            # - the tag is viewed on a monitor that is not focused
+            echo -n "%{F-}%{B#764E29}"
+            ;;
+        *)
+            # . the tag is empty
+            echo -n "%{F#AAAAAA}%{B-}"
+            ;;
+    esac
 
-        echo "%{A1:herbstclient use ${tag:1}:}${padding}${tag:1}${padding}%{A F- B-}"
-    done
-} | tr -d "\n"
+    echo -n "%{A1:herbstclient use ${tag:1}:}${padding}${tag:1}${padding}%{A F- B-}"
+done
 
