a8ed3878e100014846555a0c840308497e16d52a
HongLong40 <37321971+HongLong40@users.noreply.github.com>
2022-10-07 00:24:09 +0200

    removed leftwm configs; updated polybar/get_info_pacman_updates.sh x,y-offsets

--- .config/polybar/get_info_pacman_updates.sh
+++ .config/polybar/get_info_pacman_updates.sh
@@ -21,8 +21,8 @@ awk '{ printf("%2s %s\n", NR, $0) }' /tmp/pacman_updates | \
     rofi \
     -dmenu \
     -i \
-    -xoffset -6 \
-    -yoffset 30 \
+    -xoffset -8 \
+    -yoffset 32 \
     -p "Pacman and AUR Updates" \
     -no-show-icons \
     -theme-str "entry {enabled: false;} window {width: 25%; location: north east;} listview {lines: ${_lines}; spacing: 0px;}"
