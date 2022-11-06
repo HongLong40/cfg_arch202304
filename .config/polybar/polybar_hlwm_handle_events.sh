f9ee594c975590eb7115cce89ec3afc7f4a20a55
HongLong40 <37321971+HongLong40@users.noreply.github.com>
2022-11-06 16:08:55 +0100

    updated script: use pgrep instead of kill -0

--- .config/polybar/polybar_hlwm_handle_events.sh
+++ .config/polybar/polybar_hlwm_handle_events.sh
@@ -6,12 +6,11 @@ typeset PIDFILE="/tmp/polybar_hlwm_handle_events.pid"
 # check if script is already running
 if [[ -f $PIDFILE ]]
 then
-    pid=$(<${PIDFILE})
-
-    if kill -0 $pid
+    if pgrep -F $PIDFILE > /dev/null
     then
+        pid=$(<${PIDFILE})
         echo "[$(date)] : $PROGNAME: Process is already running with PID $pid"
-        exit 1
+        return 1
     fi
 fi
 
@@ -32,15 +31,11 @@ herbstclient --idle | {
         if [[ ${event[1]} == "attribute_changed" ]]
         then
             polybar-msg action "#hlwm_layout.hook.0" 1> /dev/null  2>&1
-            # polybar-msg hook hlwm_layout 1 1> /dev/null  2>&1
-            # sleep 0.1 # temp. fix to ensure next msg is processed by polybar
         fi
 
         if [[ ${event[1]} =~ "tag_" ]]
         then
             polybar-msg action "#hlwm_workspaces.hook.0" 1> /dev/null  2>&1
-            #polybar-msg hook hlwm_workspaces 1 1> /dev/null  2>&1
-            # sleep 0.1 # temp. fix to ensure next msg is processed by polybar
         fi
     done
 
